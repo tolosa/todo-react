b4f1e437fb2c747ff6276054e82ae00145fa25ff
Leonardo Tolosa <tolosa@gmail.com>
2019-06-04 20:50:31 -0300

    Render todos list from array

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Header from './Header/header';
 import TodoList from './TodoList/todolist';
 import Todo from './Todo/todo';
 
-function App() {
-  return (
-    <div className="App">
-      <Header title="Awesome React To-Do!" />
-      <TodoList>
-        <Todo title="First task! Awesome!" />
-        <Todo title="Second task" />
-        <Todo title="A third task" />
-      </TodoList>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: [
+      "First task! Awesome!",
+      "Second task",
+      "A third task"
+    ]
+  };
+  renderTodos() {
+    return this.state.tasks.map((task) =>
+      <Todo title={task} />
+    );
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header title="Awesome React To-Do!" />
+        <TodoList>
+          {this.renderTodos()}
+        </TodoList>
+      </div>
+    );
+  }
 }
 
 export default App;
