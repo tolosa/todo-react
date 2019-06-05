08698e11e4d7016ec0c9b8d28f7ec526d297fbb6
Leonardo Tolosa <tolosa@gmail.com>
2019-06-04 23:47:43 -0300

    Clear input text after adding task

--- src/NewTodoForm/newtodoform.js
+++ src/NewTodoForm/newtodoform.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 class NewTodoForm extends Component {
   state = { text: '' };
   handleChange = (e) => {
-    this.setState({ text: e.target.value });
+    this.setTextState(e.target.value);
+  }
+  handleAddClick = () => {
+    this.props.onAdd(this.state.text);
+    this.setTextState('');
+  }
+  setTextState(text) {
+    this.setState({text});
   }
   render() {
     return(
       <div class="card mx-3 my-4">
         <div class="card-body">
           <h5 class="card-title">Add new task</h5>
-          <input onChange={this.handleChange} />
-          <button onClick={() => this.props.onAdd(this.state.text)}>Add</button>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button onClick={this.handleAddClick}>Add</button>
         </div>
       </div>
     );
