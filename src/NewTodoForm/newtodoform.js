cbd49cce644a6370fef5f31d707550d99381a226
Leonardo Tolosa <tolosa@gmail.com>
2019-06-05 00:01:06 -0300

    Improve form styling

--- src/NewTodoForm/newtodoform.js
+++ src/NewTodoForm/newtodoform.js
@@ -14,11 +14,13 @@ class NewTodoForm extends Component {
   }
   render() {
     return(
-      <div class="card mx-3 my-4">
+      <div class="card bg-light mx-3 my-4">
         <div class="card-body">
           <h5 class="card-title">Add new task</h5>
-          <input onChange={this.handleChange} value={this.state.text} />
-          <button onClick={this.handleAddClick}>Add</button>
+          <div class="form-group">
+            <input onChange={this.handleChange} value={this.state.text} class="form-control form-control-lg" />
+          </div>
+          <button onClick={this.handleAddClick} class="btn btn-primary">Add task</button>
         </div>
       </div>
     );
