502de3bd00556e82e4cd48154df0793a3d9ab575
Leonardo Tolosa <tolosa@gmail.com>
2019-09-04 17:20:10 -0300

    Move FA imports to index

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import TodoList from './TodoList/todolist';
 import Todo from './Todo/todo';
 import NewTodoForm from './NewTodoForm/newtodoform';
 
-// FontAwesome imports
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faTimesCircle);
-
 class App extends Component {
   state = { tasks: [] };
   handleOnAdd = (task) => {
