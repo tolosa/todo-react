cdb9d6561d41287b9c9357b3c4acfa500c2cda0e
Leonardo Tolosa <tolosa@gmail.com>
2019-09-04 15:41:51 -0300

    Show default empty message

--- src/TodoList/todolist.js
+++ src/TodoList/todolist.js
@@ -3,9 +3,13 @@ import React from 'react';
 const TodoList = (props) => {
   return(
     <div className="card">
-      <ul className="list-group list-group-flush">
-        {props.children}
-      </ul>
+      { props.children.length ? (
+        <ul className="list-group list-group-flush">
+          {props.children}
+        </ul>
+      ) : (
+        <span className="lead text-muted text-uppercase p-3">No tasks to show</span>
+      ) }
     </div>
   );
 }
