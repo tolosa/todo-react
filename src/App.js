26efee61bb11b5ae46fec31b95a229ae97f95363
Leonardo Tolosa <tolosa@gmail.com>
2019-09-04 16:33:48 -0300

    Import FA components and icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// App components
 import Header from './Header/header';
 import TodoList from './TodoList/todolist';
 import Todo from './Todo/todo';
 import NewTodoForm from './NewTodoForm/newtodoform';
+
+// FontAwesome imports
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTimesCircle);
 
 class App extends Component {
   state = { tasks: [] };
