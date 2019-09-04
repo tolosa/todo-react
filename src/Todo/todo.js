5067cd5509f1f5259b52bb2e7bcda733b19bb35c
Leonardo Tolosa <tolosa@gmail.com>
2019-09-04 16:37:50 -0300

    Show delete button

--- src/Todo/todo.js
+++ src/Todo/todo.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Todo extends Component {
   render() {
-    return <li className="list-group-item">{this.props.title}</li>;
+    return (
+      <li className="list-group-item d-flex align-items-center">
+        {this.props.title}
+        <FontAwesomeIcon icon="times-circle" size="lg" className="text-danger ml-auto" />
+      </li>
+    );
   }
 }
 
