afc85fab2c41b0444115e20bbacb060bc393f1f7
Leonardo Tolosa <tolosa@gmail.com>
2019-09-14 23:30:21 -0300

    Allow to enter edit mode

--- src/Todo/InPlaceEdit/inplaceedit.js
+++ src/Todo/InPlaceEdit/inplaceedit.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
-class InPlaceEdit extends Component {
-  render() {
-    return(
-     this.props.value
-    );
+const InPlaceEdit = (props) => {
+  const [isEditModeState, setEditModeState] = useState(false);
+
+  const handleOnEdit = (editing) => {
+    setEditModeState(editing);
   }
+
+  return(
+    isEditModeState ? (
+      <div>
+        <input type="text" />
+        <FontAwesomeIcon icon={faTimesCircle} onClick={() => handleOnEdit(false)} />
+      </div>
+    ) : (
+      <div>
+        {props.value}
+        <FontAwesomeIcon icon={faEdit} onClick={() => handleOnEdit(true)} />
+      </div>
+    )
+  );
 }
 
 export default InPlaceEdit;
