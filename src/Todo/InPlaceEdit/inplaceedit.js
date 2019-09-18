cc5f085ccde968f86513a71059190a637d07c958
Leonardo Tolosa <tolosa@gmail.com>
2019-09-18 16:15:39 -0300

    Avoid container duplication

--- src/Todo/InPlaceEdit/inplaceedit.js
+++ src/Todo/InPlaceEdit/inplaceedit.js
@@ -23,28 +23,28 @@ const InPlaceEdit = (props) => {
   }
 
   const renderViewMode = () => (
-    // TODO: avoid container duplication
-    <div class="inplaceedit form-group d-block flex-grow-1">
+    <>
       <div class="form-control-plaintext">{props.value}</div>
       <div class="actions">
         <FontAwesomeIcon icon={faEdit} onClick={() => handleOnEdit(true)} />
       </div>
-    </div>
+    </>
   )
 
   const renderEditMode = () => (
-    // TODO: avoid container duplication
-    <div class="inplaceedit form-group d-block flex-grow-1">
+    <>
       <input type="text" value={newValueState} onChange={handleValueEdit} class="form-control" />
       <div class="actions">
         <FontAwesomeIcon icon={faCheckCircle} onClick={handleAcceptEdit} />
         <FontAwesomeIcon icon={faTimesCircle} onClick={() => handleOnEdit(false)} />
       </div>
-    </div>
+    </>
   )
 
   return(
-    isEditModeState ? renderEditMode() : renderViewMode()
+    <div class="inplaceedit form-group d-block flex-grow-1">
+      { isEditModeState ? renderEditMode() : renderViewMode() }
+    </div>
   );
 }
 
