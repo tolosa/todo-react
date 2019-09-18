8aec2872136174ff9d16e639ae9f1972038941bc
Leonardo Tolosa <tolosa@gmail.com>
2019-09-18 15:56:20 -0300

    Add basic layout for edit mode

--- src/Todo/InPlaceEdit/inplaceedit.js
+++ src/Todo/InPlaceEdit/inplaceedit.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
+import './inplaceedit.css';
+
 const InPlaceEdit = (props) => {
   const [isEditModeState, setEditModeState] = useState(false);
   const [newValueState, setNewValueState] = useState();
@@ -28,10 +30,12 @@ const InPlaceEdit = (props) => {
   )
 
   const renderEditMode = () => (
-    <div>
-      <input type="text" value={newValueState} onChange={handleValueEdit} />
-      <FontAwesomeIcon icon={faCheckCircle} onClick={handleAcceptEdit} />
-      <FontAwesomeIcon icon={faTimesCircle} onClick={() => handleOnEdit(false)} />
+    <div class="inplaceedit form-group d-block flex-grow-1">
+      <input type="text" value={newValueState} onChange={handleValueEdit} class="form-control" />
+      <div class="actions">
+        <FontAwesomeIcon icon={faCheckCircle} onClick={handleAcceptEdit} />
+        <FontAwesomeIcon icon={faTimesCircle} onClick={() => handleOnEdit(false)} />
+      </div>
     </div>
   )
 
