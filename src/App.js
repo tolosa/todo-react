e7c94b9c7c68f9f378168484c90de5a8ae30346c
Leonardo Tolosa <tolosa@gmail.com>
2019-11-18 11:12:24 -0300

    Delete unused code

--- src/App.js
+++ src/App.js
@@ -8,27 +8,6 @@ import NewTodoForm from './components/NewTodoForm/newtodoform';
 import './App.css';
 
 class App extends Component {
-  state = { tasks: null };
-
-  // TODO: move all this to redux
-  // componentDidMount() {
-  //   axios.get('/tasks.json', this.state.tasks)
-  //     .then(response => {
-  //       this.setState({ tasks: response.data || [] });
-  //     });
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.tasks && prevState.tasks !== this.state.tasks)
-  //     axios.put('/tasks.json', this.state.tasks);
-  // }
-
-  // updateTasks(proc) {
-  //   const tasks = this.state.tasks.splice(0);
-  //   proc(tasks);
-  //   this.setState({ tasks });
-  // }
-
   render() {
     return (
       <div className="App">
