fcd816fd123f5256a2d5adbf6ee8f9481f9be856
Leonardo Tolosa <tolosa@gmail.com>
2019-11-18 01:53:26 -0300

    Syntax changes

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import * as actions from './actionTypes';
 
-export const fetchTasks = () => ((dispatch) =>
+export const fetchTasks = () => (dispatch) =>
   axios
     .get('/tasks.json')
     .then(response =>
       dispatch(setTasks(response.data || []))
-    )
-);
+    );
 
 export const setTasks = (tasks) => ({
   type: actions.SET_TASKS,
@@ -19,22 +18,18 @@ const dispatchAndPut = (type, payload, dispatch, getState) => {
   axios.put('/tasks.json', getState().tasks);
 };
 
-export const addTask = (text) => (
+export const addTask = (text) =>
   (dispatch, getState) =>
-    dispatchAndPut(actions.ADD_TASK, { text }, dispatch, getState)
-);
+    dispatchAndPut(actions.ADD_TASK, { text }, dispatch, getState);
 
-export const changeStatus = (index, isDone) => (
+export const changeStatus = (index, isDone) =>
   (dispatch, getState) =>
-    dispatchAndPut(actions.CHANGE_STATUS, { index, isDone }, dispatch, getState)
-);
+    dispatchAndPut(actions.CHANGE_STATUS, { index, isDone }, dispatch, getState);
 
-export const edit = (index, text) => (
+export const edit = (index, text) =>
   (dispatch, getState) =>
-    dispatchAndPut(actions.EDIT, { index, text }, dispatch, getState)
-);
+    dispatchAndPut(actions.EDIT, { index, text }, dispatch, getState);
 
-export const remove = (index) => (
+export const remove = (index) =>
   (dispatch, getState) =>
-    dispatchAndPut(actions.DELETE, { index }, dispatch, getState)
-);
+    dispatchAndPut(actions.DELETE, { index }, dispatch, getState);
