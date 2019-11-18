361f37c598cb112fc3417adbe7d398e937fc4266
Leonardo Tolosa <tolosa@gmail.com>
2019-11-18 01:46:57 -0300

    Refactor to reduce duplication

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import * as actions from './actionTypes';
 
-const putTasks = (getState) =>
-  axios.put('/tasks.json', getState().tasks);
-
 export const fetchTasks = () => ((dispatch) =>
   axios
     .get('/tasks.json')
@@ -17,22 +14,27 @@ export const setTasks = (tasks) => ({
   tasks,
 });
 
-export const addTask = (text) => ((dispatch, getState) => {
-  dispatch({ type: actions.ADD_TASK, text });
-  putTasks(getState);
-});
+const dispatchAndPut = (type, payload, dispatch, getState) => {
+  dispatch({ type, ...payload });
+  axios.put('/tasks.json', getState().tasks);
+};
 
-export const changeStatus = (index, isDone) => ((dispatch, getState) => {
-  dispatch({ type: actions.CHANGE_STATUS, index, isDone });
-  putTasks(getState);
-});
+export const addTask = (text) => (
+  (dispatch, getState) =>
+    dispatchAndPut(actions.ADD_TASK, { text }, dispatch, getState)
+);
 
-export const edit = (index, text) => ((dispatch, getState) => {
-  dispatch({ type: actions.EDIT, index, text });
-  putTasks(getState);
-});
+export const changeStatus = (index, isDone) => (
+  (dispatch, getState) =>
+    dispatchAndPut(actions.CHANGE_STATUS, { index, isDone }, dispatch, getState)
+);
 
-export const remove = (index) => ((dispatch, getState) => {
-  dispatch({ type: actions.DELETE, index });
-  putTasks(getState);
-});
+export const edit = (index, text) => (
+  (dispatch, getState) =>
+    dispatchAndPut(actions.EDIT, { index, text }, dispatch, getState)
+);
+
+export const remove = (index) => (
+  (dispatch, getState) =>
+    dispatchAndPut(actions.DELETE, { index }, dispatch, getState)
+);
